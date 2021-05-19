82a384c48a5530e82798c7e25e11fb3e81b474f0
zotyev <mbixjkee1392@gmail.com>
2021-05-19 10:16:35 +0300

    Рефакторинг и тесты

--- examples/cat_facts/lib/interactor/theme/theme_interactor.dart
+++ examples/cat_facts/lib/interactor/theme/theme_interactor.dart
@@ -13,17 +13,16 @@
 // limitations under the License.
 
 import 'package:cat_facts/data/theme/app_theme.dart';
-import 'package:cat_facts/interactor/api_client.dart';
 import 'package:relation/relation.dart';
 
-class AppStorage {
+/// Интерактор для работы с темой приложения.
+class ThemeInteractor {
   final appTheme = StreamedState<AppTheme>(AppTheme.light);
-
-  final ApiClient apiClient = ApiClient();
 
   void changeTheme() {
     final current = appTheme.value;
-    appTheme
-        .accept((current == AppTheme.dark) ? AppTheme.light : AppTheme.dark);
+    appTheme.accept(
+      (current == AppTheme.dark) ? AppTheme.light : AppTheme.dark,
+    );
   }
 }
