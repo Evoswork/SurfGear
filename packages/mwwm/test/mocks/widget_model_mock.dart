b68129932a213eeee75e81bab69a93472b9ca69a
evgenii <godfire@list.ru>
2021-12-18 10:51:17 +0300

    переименовл название метода до высова initState и обязательныую функцию в duFuture

--- packages/mwwm/test/mocks/widget_model_mock.dart
+++
@@ -1,7 +0,0 @@
-import 'package:mwwm/src/dependencies/wm_dependencies.dart';
-import 'package:mwwm/src/widget_model.dart';
-
-class WidgetModelMock extends WidgetModel {
-  WidgetModelMock(WidgetModelDependencies baseDependencies)
-      : super(baseDependencies);
-}
